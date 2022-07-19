17fa5c2a249787f4c21f87f25d5adb13ad329412
Alex Borges <aborgescolon@gmail.com>
2022-07-19 14:43:18 -0400

    changed animations in header

--- components/Header.tsx
+++ components/Header.tsx
@@ -12,16 +12,16 @@ import { AiOutlineTwitter } from 'react-icons/ai';
             </Link>
             <div className="hidden md:inline-flex items-center space-x-5">
                 <h3>About</h3>
-                <h3 className="bg-green-500 text-white px-4 py-1 rounded-full hover:animate-bounce cursor-pointer">Contact</h3>
+                <h3 className="bg-green-500 text-white px-4 py-1 rounded-full cursor-pointer">Contact</h3>
             </div>
         </div>
 
         <div className="flex items-center space-x-7 cursor-pointer">
             <Link href='https://www.instagram.com/xanderborges/'>
-            <AiOutlineInstagram className="w-6 h-6  hover:text-green-500 transition-all hover:animate-bounce " />
+            <AiOutlineInstagram className="w-6 h-6  hover:text-green-500 transition-all " />
             </Link>
-            <AiOutlineFacebook className="w-6 h-6  hover:text-green-500 transition-all hover:animate-bounce "/>
-            <AiOutlineTwitter className="w-6 h-6  hover:text-green-500 transition-all hover:animate-bounce "/>
+            <AiOutlineFacebook className="w-6 h-6  hover:text-green-500 transition-all "/>
+            <AiOutlineTwitter className="w-6 h-6  hover:text-green-500 transition-all "/>
         </div>
     </header>
   )
